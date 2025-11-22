ba573a004496bcc739405e5b3535e31a382dffa5
ehy0414 <djagusdyd78@gmail.com>
2025-11-22 22:40:43 +0900

    feat: 결과 페이지 router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import QuestionPage from './pages/QuestionPage';
+import QuestionResultPage from './pages/QuestionResultPage';
 
 function App() {
     return (
